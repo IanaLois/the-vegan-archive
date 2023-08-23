c9a7973dc32cad5c18f7c67cf1a2a90a4f20c154
IanaLois <>
2023-08-23 22:04:52 +0000

    Enhance naming conventions for better readability

--- recipes/models.py
+++ recipes/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0, "Draft"), (1, "Published"))
+RECIPE_STATUS = ((0, "Draft"), (1, "Published"))
+
+APPROVAL_STATUS = ((False, "Pending Approval"), (True, "Approved"))
 
 
 class Recipe(models.Model):
@@ -14,22 +16,21 @@ class Recipe(models.Model):
     total_time = models.PositiveIntegerField(blank=False)
     serves = models.PositiveIntegerField(blank=False)
     image = CloudinaryField('image', 'recipe_placeholder', blank=False)
-    published_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
+    published_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_featured = models.BooleanField(default=False)
     featured_image = CloudinaryField(
         'image', default='landing_page_placeholder', blank=False)
     featured_description = models.TextField(blank=False)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(
+    liked_by = models.ManyToManyField(
         User, blank=True, related_name='liked_recipes')
     calories = models.PositiveIntegerField(blank=False)
     is_approved = models.BooleanField(default=False)
-    
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ["-published_on"]
 
     def __str__(self):
         return self.title
