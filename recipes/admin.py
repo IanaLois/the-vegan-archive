2544ab3764b048c1ce29a44b7f72295bb06dd91c
IanaLois <>
2023-08-24 10:16:34 +0000

    Implement recipe approval functionality in recipeadmin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Like, Profile
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
+    
+    list_display = ('title', 'slug', 'status', 'published_on', 'updated_on', 'is_approved')
+    search_fields = ['title', 'description']
     prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('status', 'published_on')
-    summernote_fields = ('ingredients', 'instructions',)
+    list_filter = ('status', 'published_on', 'is_approved')
+    summernote_fields = ('description', 'ingredients', 'instructions',)
+    actions = ['approve_recipes']
+
+    def approve_recipes(self, request, queryset):
+        queryset.update(is_approved=True)
