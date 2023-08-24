5ea260c3658b206f663756fd66f555cdcc0ccf83
IanaLois <>
2023-08-24 09:13:40 +0000

    Enable null values in slug field to fix migration error

--- recipes/admin.py
+++ recipes/admin.py
@@ -5,4 +5,6 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'published_on')
     summernote_fields = ('ingredients', 'instructions',)
--- recipes/models.py
+++ recipes/models.py
@@ -10,7 +10,7 @@ APPROVAL_STATUS = ((False, "Pending Approval"), (True, "Approved"))
 class Recipe(models.Model):
     recipe = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=200, blank=False)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, null=True)
     image = CloudinaryField('image', 'recipe_placeholder', blank=False)
     description = models.TextField(blank=False)
     serves = models.PositiveIntegerField(blank=False)
